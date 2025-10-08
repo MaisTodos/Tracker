f934a960c4639c6eafc5cc290767ad7aedbe2ad7
Johnathan Barbosa <johnathan.araujo@maistodos.com.br>
2025-10-08 15:00:11 -0300

    cicd: format black

--- tracker/interfaces.py
+++ tracker/interfaces.py
@@ -6,27 +6,27 @@ from .types import Contexts, Tags
 
 class ISetMixin(ABC):
     @abstractmethod
-    def set_tags(self, tags: Tags):
+    def set_tags(self, tags: Tags):  # pragma: no cover
         ...
 
     @abstractmethod
-    def set_contexts(self, contexts: Contexts):
+    def set_contexts(self, contexts: Contexts):  # pragma: no cover
         ...
 
 
 class ITrackerHandlerException(ISetMixin, ABC):
     @abstractmethod
-    def capture_exception(self, tracker_exception: TrackerException):
+    def capture_exception(self, tracker_exception: TrackerException):  # pragma: no cover
         ...
 
 
 class ITrackerHandlerMessage(ISetMixin, ABC):
     @abstractmethod
-    def capture_message(self, tracker_message: TrackerMessage):
+    def capture_message(self, tracker_message: TrackerMessage):  # pragma: no cover
         ...
 
 
 class ITrackerHandlerEvent(ISetMixin, ABC):
     @abstractmethod
-    def capture_event(self, tracker_event: TrackerEvent):
+    def capture_event(self, tracker_event: TrackerEvent):  # pragma: no cover
         ...
